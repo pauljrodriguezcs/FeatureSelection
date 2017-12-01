ae783e2055d6fad0fbf219db39c88ecbc880b2ac
Paul Rodriguez <prodr010@ucr.edu>
2017-12-01 11:35:41 -0800

    cleaned up code

--- featureSelection.java
+++ featureSelection.java
@@ -9,15 +9,15 @@ public class featureSelection {
         public static int numFeatures = 0;  //number of features(x value)
         public static int numItems = 0;     //number of items in our data set
         public static int numLines = 0;     //number of lines(y value)
-        public static double totalValue = 0.0;  //sum of items in our data set
-        public static double meanValue = 0.0;   //average value of items
-        public static double variance = 0.0;
-        public static double stdDev= 0.0;
         public static boolean numFeatureSet = false;    //will be used to distinguish first line when reading the file
         public static String delims = "[ ]+";   //used to split the line
         public static String[] tokens;      //will hold the items that are read in
         
         public static List<List<Double>> dataList = new ArrayList<List<Double>>(numFeatures);
+        public static List<Double> totalValue = new ArrayList<Double>(numFeatures);
+        public static List<Double> meanValue = new ArrayList<Double>(numFeatures);
+        public static List<Double> variance = new ArrayList<Double>(numFeatures);
+        public static List<Double> stdDev = new ArrayList<Double>(numFeatures);
     }
     
     public static class best{
@@ -44,11 +44,7 @@ public class featureSelection {
                     for(int i = 0; i < gv.numFeatures + 1; ++i){
                         gv.dataList.add(new ArrayList<Double>());
                         double tmpDouble = Double.parseDouble(gv.tokens[i+1]);
-                        
-                        if(i != 0){                 //prevents adding the class identifer into totalValue
-                            gv.totalValue = gv.totalValue + tmpDouble;
-                            ++gv.numItems;
-                        }
+                        gv.totalValue.add(tmpDouble);  //new items
                         gv.dataList.get(i).add(tmpDouble);
                     }//end for loop
                 }//end if statement
@@ -57,8 +53,7 @@ public class featureSelection {
                     for(int i = 0; i < gv.numFeatures+1; ++i){
                         double tmpDouble = Double.parseDouble(gv.tokens[i+1]);
                         if(i != 0){
-                            gv.totalValue = gv.totalValue + tmpDouble;
-                            ++gv.numItems;
+                            gv.totalValue.set(i,gv.totalValue.get(i) + tmpDouble);  //new items
                         }
                         gv.dataList.get(i).add(tmpDouble);
                     }//end for loop
@@ -72,22 +67,39 @@ public class featureSelection {
     }
     
     public static void normalize_data(){
-        gv.meanValue = gv.totalValue / gv.numItems; //calculate the meanvalue
         
-        for(int i = 1; i < gv.numFeatures+1; ++i){  //iterate through dataset and find variance sum
+        for(int i = 0; i < gv.numFeatures + 1; ++i){                //new items
+            gv.meanValue.add(gv.totalValue.get(i)/gv.numLines);      //new items
+            StdOut.println(gv.meanValue.get(i));
+        }                                                           //new items
+        
+        for(int i = 0; i < gv.numFeatures+1; ++i){  //iterate through dataset and find variance sum
             for(int j = 0; j < gv.numLines; ++j){
-                gv.variance = gv.variance + Math.pow((gv.dataList.get(i).get(j) - gv.meanValue),2);
+                if(j == 0){
+                    gv.variance.add(Math.pow((gv.dataList.get(i).get(j) - gv.meanValue.get(i)),2)); //new items
+                }
+                
+                else{
+                    gv.variance.set(i,gv.variance.get(i) + Math.pow((gv.dataList.get(i).get(j) - gv.meanValue.get(i)),2)); //new items
+                }
+                
             }
+            StdOut.println(gv.variance.get(i));
         }
         
-        gv.variance = gv.variance/(gv.numItems-1);
-        gv.stdDev = Math.sqrt(gv.variance);
+        for(int i = 0; i < gv.numFeatures + 1; ++i){
+            gv.variance.set(i,gv.variance.get(i)/(gv.numLines-1));
+            gv.stdDev.add(Math.sqrt(gv.variance.get(i)));
+            StdOut.println(gv.stdDev.get(i));
+            
+        }
         
         for(int i = 1; i < gv.numFeatures+1; ++i){  //z-normalize the data (x-mean)/stdDev
             for(int j = 0; j < gv.numLines; ++j){
-                gv.dataList.get(i).set(j,(gv.dataList.get(i).get(j) - gv.meanValue)/gv.stdDev);
+                gv.dataList.get(i).set(j,(gv.dataList.get(i).get(j) - gv.meanValue.get(i))/gv.stdDev.get(i));
             }
         }
+    
     }
     
     public static double leave_one_out_cross_validation(Set<Integer> current_set_of_features,int k){
@@ -132,91 +144,39 @@ public class featureSelection {
             }
         }
         
-        
-        /*
-        if(current_set_of_features.isEmpty()){
-            for(int i = 0; i < gv.numLines; ++i){
-                for(int j = 0; j < gv.numLines; ++j){
-                    if(i != j){
-                        double tmp_nn = Math.abs(gv.dataList.get(k).get(j) - gv.dataList.get(k).get(i));
-                        if(tmp_nn < nearest_neighbor){
-                            nearest_neighbor = tmp_nn;
-                            nn_index = j;
-                        } //end if
-                    } // end if
-                } //end for
-                
-                if(Math.floor(gv.dataList.get(0).get(i)) == Math.floor(gv.dataList.get(0).get(nn_index))){
-                    ++correct_classification;
-                } // end if
-                
-                nearest_neighbor = Double.POSITIVE_INFINITY;
-            }// end for
-        }
-        
-        else{
-            Iterator<Integer> it = current_set_of_features.iterator();
-            Integer[] cf = new Integer[current_set_of_features.size()];
-            for(int i = 0; i < cf.length; ++i){
-                cf[i] = it.next();
-            }
-            ArrayList<Double> sums = new ArrayList<Double>();
-            
-            for(int i = 0; i < cf.length; ++i){
-                for(int j = 0; j < gv.numLines; ++j){
-                    if(i == 0){
-                       // StdOut.print("i = 0: ");
-                        sums.add(gv.dataList.get(cf[i]).get(j));
-                        //StdOut.println(sums.get(j));
-                        
-                    }
-                    //StdOut.println("i = " + i);
-                    sums.set(j,sums.get(j) + gv.dataList.get(k).get(j));
-                }
-                
-            }
-            
-            for(int i = 0; i < gv.numLines; ++i){
-                for(int j = 0; j < gv.numLines; ++j){
-                    if(i != j){
-                        double tmp_nn = Math.abs(sums.get(j) - sums.get(i));
-                        if(tmp_nn < nearest_neighbor){
-                            nearest_neighbor = tmp_nn;
-                            nn_index = j;
-                        } //end if
-                    } // end if
-                } //end for
-                
-                if(Math.floor(gv.dataList.get(0).get(i)) == Math.floor(gv.dataList.get(0).get(nn_index))){
-                    ++correct_classification;
-                } // end if
-                
-                nearest_neighbor = Double.POSITIVE_INFINITY;
-            }// end for
-            
-            
-            
-            
-        }
-        */
         return correct_classification/gv.numLines;
-        
-        
-        
     }
     
     
     public static void main(String[] args){
+        int choice = 0;
+        boolean proceed = false;
         StdOut.println('\n' + "Welcome to Paul's Feature Selection Algorithm.");
         StdOut.print('\n' + "Type in the name of the file to test: ");
         String textFileName = StdIn.readLine();     //read in name of file
+        
+        do{
+            StdOut.println("Type the number of the algorithm you want to run.");
+            StdOut.println("    1)Forward Selection");
+            StdOut.println("    2)Backward Elimination");
+            StdOut.println("    3)Bertie’s Special Algorithm");
+            choice = StdIn.readInt();
+            
+            if(choice == 1 || choice == 2 || choice == 3){
+                proceed = true;
+            }
+            
+            else{
+                StdOut.println("Error! Enter correct choice!");
+            }
+            
+        }while(!proceed);
+        
         fill_array(textFileName);                   //fill list with file values
         StdOut.println('\n' + "This dataset has " + gv.numFeatures + " features (not including the class attribute), with " + gv.numLines + " instances.");
         StdOut.print('\n' + "Please wait while I normalize the data…   ");
         normalize_data();                           //normalize the data
         StdOut.println("Done!" + '\n');
-        
-        
         
         Set<Integer> current_set_of_features = new HashSet<Integer>();
         for(int i = 0; i < gv.numFeatures; ++i){
@@ -228,6 +188,7 @@ public class featureSelection {
                 if(!current_set_of_features.contains(j+1)){
                     StdOut.print("--Considering adding the " + (j+1) + " feature: ");
                     double accuracy = leave_one_out_cross_validation(current_set_of_features,j+1);
+    
                     StdOut.println(accuracy + "% accuracy");
                     
                     if(accuracy > best_so_far_accuracy){
@@ -264,7 +225,9 @@ public class featureSelection {
         StdOut.println("} with accuracy " + best.accuracy);
         
         //CS170Smalltestdata__53.txt {1 2} with accuracy 0.96 =     2 7 1
-        //CS170BIGtestdata__98.txt {19 39 44} with accuracy 0.95 =  19 9 39
+        //CS170BIGtestdata__98.txt {19 39} with accuracy 0.94 =  19 9 39
+        //CS170Smalltestdata__22.txt {4 8} with accuracy 0.92
+        //CS170BIGtestdata__37.txt {37 46} with accuracy 0.97
         
         
     }
